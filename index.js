61a836c92633e6c5cfc4a0782158f738898723ce
joelgbs <115678569+joelgbs@users.noreply.github.com>
2023-07-26 20:47:58 -0500

    cargado

---
+++ index.js
@@ -0,0 +1,16 @@
+function mostrar(){
+    var texto1 = "SOLO TE QUERIA RECORDAR QUE ESTOY MUY ENAMORADO DE TI";
+    var texto2  =  "TE AMO";
+    var frase = "Eres la forma más bonita que ha tenido la vida de enseñarme que vale la pena arriesgarse a amar"
+    var frase2 = "Si quieres saber cuando te amo, solo cuenta las estrellas";
+    var frase3 = "Te amo en colores que aún no has visto";
+    var frase4 = "Te cruzaste en mi camino y se me olvidó a dónde iba";
+    var frase5 = "Haces mi mundo más bonito, aunque no lo sepas";
+    alert(texto1);
+    alert(texto2);
+    alert(frase);
+    alert(frase2);
+    alert(frase3);
+    alert(frase4);
+    alert(frase5);
+}
